5c6ce9566d147362eed4b0501f58468f1e2cb307
Hosea <karanja.mungai23@gmail.com>
2024-11-29 12:30:06 +0300

    passed tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    
+    def __init__(self, word):
+        self.word =word
+        
+    def match(self, list):
+        matched_words= []
+        for list_item in list:
+            if sorted(list_item) == sorted(self.word):
+                matched_words.append(list_item)
+        self.match= matched_words
+
+        if len(self.match) >= 1:
+            return self.match
+        else:
+            return[]
